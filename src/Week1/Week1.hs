f2d6a6e0ec255bbdc572af3cc76320fc0eb4eda9
Hans Fredrik Furholt <hansff@gmail.com>
2022-12-01 18:08:39 +0100

    day 1

---
+++ src/Week1/Week1.hs
@@ -0,0 +1,36 @@
+module Week1.Week1 (part1, part2) where
+
+import Data.List.Split (splitOn)
+import Data.List (sort)
+
+part1 :: IO ()
+part1 = readFile filename >>= putStrLn . processInput1 . lines
+  where
+    -- filename = "src/Week1/sample.txt"
+    filename = "src/Week1/input.txt"
+
+processInput1 :: [String] -> String
+processInput1 input = show $ maximum groups
+  where
+    groups = sumGroups input
+
+part2 :: IO ()
+part2 = readFile filename >>= putStrLn . processInput2 . lines
+  where
+    -- filename = "src/Week1/sample.txt"
+    filename = "src/Week1/input.txt"
+
+processInput2 :: [String] -> String
+processInput2 input = show $ sum top3
+  where
+    groups = sumGroups input
+    top3 = take 3 $ reverse $ sort groups
+
+
+sumGroups :: [String] -> [Int]
+sumGroups input = do
+  group <- splitOn [""] input
+  return $ sum $ map readInt group
+
+readInt :: String -> Int
+readInt = read
