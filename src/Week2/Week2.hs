dec1aeb32684daac36c475263b2238140e64c3c9
Hans Fredrik Furholt <hansff@gmail.com>
2022-12-02 12:27:04 +0100

    day 2

---
+++ src/Week2/Week2.hs
@@ -0,0 +1,41 @@
+module Week2.Week2 where
+
+part1 :: IO ()
+part1 = readFile filename >>= putStrLn . processInput1 . lines
+  where
+    filename = "src/Week2/sample.txt"
+    -- filename = "src/Week2/input.txt"
+
+part2 :: IO ()
+part2 = readFile filename >>= putStrLn . processInput2 . lines
+  where
+    -- filename = "src/Week2/sample.txt"
+    filename = "src/Week2/input.txt"
+
+processInput2 :: [String] -> String
+processInput2 input = show $ sum $ map (calculateScore . convertInput) input
+
+processInput1 :: [String] -> String
+processInput1 input = show $ sum $ map calculateScore input
+
+calculateScore :: String -> Int
+calculateScore ('A' : ' ' : 'X' : []) = 3 + 1
+calculateScore ('A' : ' ' : 'Y' : []) = 6 + 2
+calculateScore ('A' : ' ' : 'Z' : []) = 0 + 3
+calculateScore ('B' : ' ' : 'X' : []) = 0 + 1
+calculateScore ('B' : ' ' : 'Y' : []) = 3 + 2
+calculateScore ('B' : ' ' : 'Z' : []) = 6 + 3
+calculateScore ('C' : ' ' : 'X' : []) = 6 + 1
+calculateScore ('C' : ' ' : 'Y' : []) = 0 + 2
+calculateScore ('C' : ' ' : 'Z' : []) = 3 + 3
+
+convertInput :: String -> String
+convertInput ('A' : ' ' : 'X' : []) = "A Z"
+convertInput ('A' : ' ' : 'Y' : []) = "A X"
+convertInput ('A' : ' ' : 'Z' : []) = "A Y"
+convertInput ('B' : ' ' : 'X' : []) = "B X"
+convertInput ('B' : ' ' : 'Y' : []) = "B Y"
+convertInput ('B' : ' ' : 'Z' : []) = "B Z"
+convertInput ('C' : ' ' : 'X' : []) = "C Y"
+convertInput ('C' : ' ' : 'Y' : []) = "C Z"
+convertInput ('C' : ' ' : 'Z' : []) = "C X"
