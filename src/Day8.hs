e7de1d835a23f97573b8d9fb513cbb5a74b80c7f
Hans Fredrik Furholt <hansff@gmail.com>
2022-12-08 17:44:57 +0100

    day 2 part 2

--- src/Day8.hs
+++ src/Day8.hs
@@ -7,13 +7,15 @@ import Utils (InputType (..), commonSolve)
 solve :: IO ()
 solve = commonSolve 8 Input part1 part2
 
+type Tree = (Int, Int)
+
 part1 :: [String] -> Int
 part1 input = length $ filter (isVisible matrix) trees
  where
   trees = [(x, y) | x <- [0 .. length (head input) - 1], y <- [0 .. length input - 1]]
   matrix = map (map digitToInt) input
 
-isVisible :: [[Int]] -> (Int, Int) -> Bool
+isVisible :: [[Int]] -> Tree -> Bool
 isVisible matrix (x, y) = isVisibleInRow row x || isVisibleInRow column y
  where
   transposed = transpose matrix
@@ -27,5 +29,23 @@ isVisibleInRow xs x = (x == 0) || (x == (length xs - 1)) || (maximum before < e)
   before = take x xs
   after = drop (x + 1) xs
 
-part2 input = "TODO"
+part2 :: [String] -> Int
+part2 input = maximum $ map (treeVisibility matrix) trees
+ where
+  trees = [(x, y) | x <- [0 .. length (head input) - 1], y <- [0 .. length input - 1]]
+  matrix = map (map digitToInt) input
 
+treeVisibility :: [[Int]] -> Tree -> Int
+treeVisibility forest (x, y) = product $ map viewedTrees [left, right, up, down]
+ where
+  transposed = transpose forest
+  row = forest !! y
+  height = row !! x
+  column = transposed !! x
+  left = reverse $ take x row
+  right = drop (x + 1) row
+  up = reverse $ take y column
+  down = drop (y + 1) column
+  viewedTrees line =
+    let untilBlocked = takeWhile (< height) line
+     in min (length line) (succ $ length untilBlocked)
