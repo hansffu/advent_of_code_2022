8b543174343763c981d0c17f58c87a84ca257519
Hans Fredrik Furholt <hansff@gmail.com>
2022-12-18 01:49:32 +0100

    wip2

--- src/Day17.hs
+++ src/Day17.hs
@@ -1,7 +1,6 @@
 module Day17 where
 
 import Control.Monad (join)
-import Data.Bitraversable (Bitraversable)
 import Utils (InputType (..), commonSolve)
 
 solve :: IO ()
@@ -25,14 +24,30 @@ stoneList = repeatList $ enumFrom Plus
 repeatList :: [a] -> [a]
 repeatList = join . repeat
 
-
 -- Ground is flipped. (coordinate !! 3) is the third column. Ground floor is index 0
 simulateFall :: StonePosition -> Ground -> Wind -> Int -> Ground
 simulateFall _ ground [] _ = ground
-simulateFall stonePosition@(stone, (x,y)) ground (wind:winds) roundsLeft
-  | hitGround stonePosition ground = ground
+simulateFall stonePosition@(stone, (x, y)) ground (wind : winds) roundsLeft
+  | roundsLeft == 0 = ground
+  | hitGround stonePosition ground =
+      simulateFall
+        (nextStonePosition ground stonePosition)
+        (addStoneToGround ground stonePosition)
+        (wind : winds)
+        (roundsLeft - 1)
+  | hitGround afterWind ground =
+      simulateFall
+        (nextStonePosition ground afterFall)
+        (addStoneToGround ground afterFall)
+        winds
+        (roundsLeft - 2)
+  | otherwise = simulateFall afterWind ground winds (roundsLeft - 1)
  where
-   afterFall = stoneCoordinates (stone, (x, y+1))
+  afterFall = (stone, (x, y + 1))
+  afterWind = case wind of
+    '>' -> (stone, (x + 1, y+1))
+    '<' -> (stone, (x - 1, y+1))
+    _ -> error "illegal input"
 
 hitGround :: StonePosition -> Ground -> Bool
 hitGround (stone, coordinate) ground = True
@@ -43,5 +58,17 @@ stoneCoordinates (stone, (x, y)) = stoneCoordinates' stone
   stoneCoordinates' Plus = [(x, y), (x + 1, y), (x + 2, y), (x + 3, y)] -- starting in the middle
   stoneCoordinates' Minus = [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)] -- starting left
   stoneCoordinates' BackL = [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)] -- starting bottom right
-  stoneCoordinates' Pipe = [(x,y), (x, y+1), (x, y+2), (x, y+3)] -- starting bottom
-  stoneCoordinates' Block = [(x,y), (x+1, y), (x, y+1), (x+1, y+1)] -- starting bottom left
+  stoneCoordinates' Pipe = [(x, y), (x, y + 1), (x, y + 2), (x, y + 3)] -- starting bottom
+  stoneCoordinates' Block = [(x, y), (x + 1, y), (x, y + 1), (x + 1, y + 1)] -- starting bottom left
+
+nextStonePosition :: Ground -> StonePosition -> StonePosition
+nextStonePosition g (stone, _) = s
+ where
+   nextStone = succStone stone
+
+addStoneToGround :: Ground -> StonePosition -> Ground
+addStoneToGround g s = g
+
+succStone :: Stone -> Stone
+succStone Block = Plus
+succStone stone = succ stone
